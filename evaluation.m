1aee4aa7bc727935d51a8e649cce0e7dfce6b4c4
cjhhjenny1 <92572899+cjhhjenny1@users.noreply.github.com>
2023-12-04 11:14:57 +0800

    Add files via upload

---
+++ evaluation.m
@@ -0,0 +1,11 @@
+function BestMea=evaluation(T,cl,n)
+import java.util.LinkedList
+import Library.*
+NMI = aNMI(T,cl);
+RI =aCalculate_Cluster_RandIndex(T,cl);
+ARI =aARI(T,cl,n);
+ACC1 =aACC1(T,cl);
+AMI =aAMI1(T,cl);
+FMI=GetFmi(T,cl);
+BestMea=[ACC1,NMI,ARI,AMI,RI,FMI];
+end
---
+++ main.m
@@ -0,0 +1,17 @@
+%% loading data
+clear all; close all; clc;
+load dataset\synthetic\D1.mat 
+data=dataset(:,1:end-1); 
+T=dataset(:,end);  % T:true lable 
+[N,dim]=size(data); % N:size Dim:dimension
+%% data preprocessing
+data=normalize(data);       
+data(isnan(data))=0;
+%% Parameter
+k=5;
+nc=3;
+%% DPC-DVND algorithm will be available after the paper is published
+[cluster,Label,rs,TotalTime]=DPC_DVND(data,N,k,nc);
+%% evaluation metrics
+BestMea=evaluation(T,Label,N)
+drawcluster(data,cluster,nc,Label);
